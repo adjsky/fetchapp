ef5738a635bfdf9de44a371cc9dbfcd9ff21cf7e
adjsky <igorlfmartins@mail.ru>
2021-03-07 21:06:59 +1000

    add token manager test

--- client/tests/tokenmanager_test.cpp
+++ client/tests/tokenmanager_test.cpp
@@ -2,12 +2,28 @@
 #include "../cpp/TokenManager/tokenmanager.hpp"
 
 TEST_CASE("TokenManager") {
-    TokenManager tokenManager{ TokenManager{} };
+    TokenManager tokenManager{ };
 
-    SECTION("Can get token after saving") {
+    SECTION("Manager returns the same token after saving") {
         QString tokenToBeSaved{ "asd" };
         tokenManager.saveToken(tokenToBeSaved);
         const QString& token{ tokenManager.getToken() };
         REQUIRE(token == tokenToBeSaved);
+        TokenManager::remove();
+    }
+
+    SECTION("Getting token when there's no saved token returns empty string") {
+        TokenManager::remove();
+        const QString& token{ tokenManager.getToken() };
+        REQUIRE(token == "");
+    }
+
+    SECTION("Can get token from file") {
+        TokenManager::remove();
+        QString tokenToSave{ "asd" };
+        tokenManager.saveToken(tokenToSave);
+        TokenManager newTokenManager{};
+        const QString& token{ newTokenManager.getToken() };
+        REQUIRE(token == tokenToSave);
     }
 }
