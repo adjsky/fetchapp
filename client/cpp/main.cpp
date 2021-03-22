f12acdb15b1b9492c4a5dbedee8de3569efab015
adjsky <igorlfmartins@mail.ru>
2021-03-22 17:07:46 +1000

    wrap raw string into qstringliteral

--- client/cpp/main.cpp
+++ client/cpp/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    const QUrl url(QStringLiteral("qrc:///main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
     }, Qt::QueuedConnection);
 
     // initialization
-    QFontDatabase::addApplicationFont(":/fonts/Roboto-Medium.ttf");
+    QFontDatabase::addApplicationFont(QStringLiteral(":/fonts/Roboto-Medium.ttf"));
     app.setOrganizationName("adjsky");
 
     // add c++ classes to qml
