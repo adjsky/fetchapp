4890b854668d096e20fb940d2ef993c02b290235
adjsky <igorlfmartins@mail.ru>
2021-03-22 18:00:32 +1000

    fix tests

--- client/tests/tokenmanager_test.cpp
+++ client/tests/tokenmanager_test.cpp
@@ -4,24 +4,24 @@
 TEST_CASE("TokenManager") {
     TokenManager tokenManager{ };
 
-    SECTION("Manager returns the same token after saving") {
+    SECTION("Manager should return the same token after saving") {
         QString tokenToBeSaved{ "asd" };
-        tokenManager.saveToken(tokenToBeSaved);
+        tokenManager.saveToken(tokenToBeSaved, true);
         const QString& token{ tokenManager.getToken() };
         REQUIRE(token == tokenToBeSaved);
         TokenManager::remove();
     }
 
-    SECTION("Getting token when there's no saved token returns empty string") {
+    SECTION("getToken when there's no saved token returns an empty string") {
         TokenManager::remove();
         const QString& token{ tokenManager.getToken() };
         REQUIRE(token == "");
     }
 
-    SECTION("Can get token from file") {
+    SECTION("Can get token from a file") {
         TokenManager::remove();
         QString tokenToSave{ "asd" };
-        tokenManager.saveToken(tokenToSave);
+        tokenManager.saveToken(tokenToSave, true);
         TokenManager newTokenManager{};
         const QString& token{ newTokenManager.getToken() };
         REQUIRE(token == tokenToSave);
