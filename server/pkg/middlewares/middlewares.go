d8ee828d93fa58cfa989726e8b1fd3adc3e78a49
adjsky <adjsky@DESKTOP-O1MLU1T>
2021-03-24 08:35:33 +1000

    add content type middleware

--- server/pkg/middlewares/middlewares.go
+++ server/pkg/middlewares/middlewares.go
@@ -1,8 +1,21 @@
 package middlewares
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"mime"
 	"net/http"
+	"server/pkg/handlers"
+	"strings"
+)
+
+// ContextKey is a type that is used to reference data in a context associated with
+type ContextKey int
+
+const (
+	// ClaimsID constant is used to reference claims in a request context
+	BoundaryID ContextKey = iota + 1
 )
 
 // Log middleware logs every incoming call to the server
@@ -11,4 +24,30 @@ func Log(next http.Handler) http.Handler {
 		log.Println(req.URL.Path, req.Method, req.UserAgent(), req.RemoteAddr)
 		next.ServeHTTP(w, req)
 	})
+}
+
+// ContentTypeValidator return middleware that check whether request's content-type header equals to required
+func ContentTypeValidator(required string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			contentType, params, _ := mime.ParseMediaType(req.Header.Get("Content-Type"))
+			if contentType == "" {
+				handlers.RespondError(w, http.StatusBadRequest, "no content-type header provided")
+				return
+			}
+			if contentType != required {
+				handlers.RespondError(w, http.StatusBadRequest, fmt.Sprintf("server accepts requests only with %s header", required))
+				return
+			}
+			if strings.HasPrefix(contentType, "multipart") {
+				boundary, ok := params["boundary"]
+				if !ok {
+					handlers.RespondError(w, http.StatusBadRequest, "no boundary provided")
+					return
+				}
+				req = req.WithContext(context.WithValue(req.Context(), BoundaryID, boundary))
+			}
+			next.ServeHTTP(w, req)
+		})
+	}
 }
