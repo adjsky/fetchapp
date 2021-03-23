532011c9fee28ffcaeb785bc2f95ac3ac274ae85
adjsky <igorlfmartins@mail.ru>
2021-03-23 10:43:01 +1000

    add function to drop scheme

--- client/qml/scripts/scripts.js
+++ client/qml/scripts/scripts.js
@@ -1,8 +1,23 @@
 function validateEmail(email) {
     let emails = /^\S+@\S+$/.exec(email)
     if (emails === null) {
-        return ""
-    } else {
-        return emails[0]
+        return false
     }
+    return true
+}
+
+function validateTextFile(filepath) {
+    let filepaths = /\.(txt)$/i.exec(filepath)
+    if (filepaths === null) {
+        return false
+    }
+    return true
+}
+
+function dropScheme(filepath) {
+    let res = filepath.split("//")
+    if (res.length === 1)  {
+        return res
+    }
+    return res[1]
 }
