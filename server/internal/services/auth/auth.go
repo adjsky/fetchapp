c1c175d7385b984a4e669435c7c0f3890ffd8d76
adjsky <lopr1965@gmail.com>
2021-03-31 18:40:47 +1000

    add token validation

--- server/internal/services/auth/auth.go
+++ server/internal/services/auth/auth.go
@@ -1,9 +1,15 @@
 package auth
 
 import (
+	"errors"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+)
+
+const (
+	tokenIssuer  string = "adjsky"
+	tokenSubject string = "auth"
 )
 
 // UserClaims holds user information passed by Authorization HTTP header
@@ -12,27 +18,27 @@ type UserClaims struct {
 	jwt.StandardClaims
 }
 
-// GenerateClaims generates new JWT token claims
+// GenerateClaims generates a new JWT token claims
 func GenerateClaims(email string) *UserClaims {
 	return &UserClaims{
 		Email: email,
 		StandardClaims: jwt.StandardClaims{
 			IssuedAt:  time.Now().Unix(),
-			Issuer:    "adjsky",
-			Subject:   "auth",
+			Issuer:    tokenIssuer,
+			Subject:   tokenSubject,
 			ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
 		},
 	}
 }
 
-// GenerateTokenString returns JWT string that is passed to a client
+// GenerateTokenString returns a JWT string that is passed to a client
 func GenerateTokenString(claims *UserClaims, secretKey []byte) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	ss, err := token.SignedString(secretKey)
 	return ss, err
 }
 
-// GetClaims decodes JWT string passed by a client and returns data associated with it
+// GetClaims decodes a JWT string passed by a client and returns data associated with it if the token is valid
 func GetClaims(tokenString string, secretKey []byte) (*UserClaims, error) {
 	token, err := jwt.ParseWithClaims(tokenString, &UserClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return secretKey, nil
@@ -41,7 +47,9 @@ func GetClaims(tokenString string, secretKey []byte) (*UserClaims, error) {
 		return nil, err
 	}
 	if claims, ok := token.Claims.(*UserClaims); ok {
-		return claims, nil
+		if claims.Issuer == tokenIssuer && claims.Subject == tokenSubject {
+			return claims, nil
+		}
 	}
-	return nil, nil
+	return nil, errors.New("token has invalid claims")
 }
