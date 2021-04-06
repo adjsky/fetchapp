2deb54706ad9ffb78db832ea08b1d685ca0a4795
adjsky <lopr1965@gmail.com>
2021-04-06 18:16:48 +1000

    clean up

--- client/qml/translations/translations.hpp
+++ client/qml/translations/translations.hpp
@@ -1,9 +1,9 @@
 // Here are strings that should be parsed by lupdate script
 
-QT_TRANSLATE_NOOP("backend", "Server is not available");
-QT_TRANSLATE_NOOP("backend", "No user registered with this email");
-QT_TRANSLATE_NOOP("backend", "Find the longest substring with the same letter");
-QT_TRANSLATE_NOOP("backend", "Find the longest substring with different letters");
-QT_TRANSLATE_NOOP("backend", "Find the longest substring with the same given letter");
+QT_TRANSLATE_NOOP("backend", "Server is not available")
+QT_TRANSLATE_NOOP("backend", "No user registered with this email")
+QT_TRANSLATE_NOOP("backend", "Find the longest substring with the same letter")
+QT_TRANSLATE_NOOP("backend", "Find the longest substring with different letters")
+QT_TRANSLATE_NOOP("backend", "Find the longest substring with the same given letter")
 QT_TRANSLATE_NOOP("backend", "Provide email and password")
 QT_TRANSLATE_NOOP("backend", "Invalid email address")
