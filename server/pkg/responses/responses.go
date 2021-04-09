3f24b9cca9fc6857cac47cf1b85c35815faa6ad6
adjsky <lopr1965@gmail.com>
2021-04-09 17:30:24 +1000

    typo

--- server/pkg/responses/responses.go
+++ server/pkg/responses/responses.go
@@ -1,6 +1,6 @@
 package responses
 
-// Error is returned when an error is occured
+// Error is returned when an error is occurred
 type Error struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
